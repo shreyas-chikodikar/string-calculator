c64b3f5d7ebcfbbf82bd31d0ab43eb3d448b04c0
shreyas-chikodikar <chikodikarshreyas98@gmail.com>
2024-10-18 21:43:51 +0530

    test for 2 or more numbers

--- spec/string-calculator.spec.js
+++ spec/string-calculator.spec.js
@@ -29,5 +29,6 @@ describe("String Calculator", () => {
 
   it("returns sum when given input of 2 or more comma-seperated numbers", () => {
     expect(add("1,2")).toBe(3);
+    expect(add("1,2,3,4,5")).toBe(15);
   });
 });
