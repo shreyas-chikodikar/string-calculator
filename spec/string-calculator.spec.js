406a5eef0722d2223a463f3728e80e900d24fe5b
shreyas-chikodikar <chikodikarshreyas98@gmail.com>
2024-10-18 19:26:17 +0530

    logic for a single number as input

--- spec/string-calculator.spec.js
+++ spec/string-calculator.spec.js
@@ -4,11 +4,15 @@ describe("String Calculator", () => {
   });
 
   it("expects string as argument", () => {
-    expect(add("0")).toBe(true);
+    expect(add("0")).toBeDefined();
     expect(add(0)).not.toBe(true);
   });
 
   it("returns 0 for empty string", () => {
     expect(add("")).toBe(0);
+  });
+
+  it("returns same number on single number input", () => {
+    expect(add("1")).toBe(1);
   });
 });
--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,6 +1,6 @@
 function add(numString) {
   if (typeof numString !== "string") return false;
-
   if (numString === "") return 0;
+  else if (numString.split(",").length == 1) return Number(numString);
   return true;
 }
